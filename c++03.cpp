ea6ecd87593ff7c475d8ad62b16aa1682ddd8d98
Karan Veer Pal <karanpalveer@gmail.com>
2024-07-23 17:35:03 +0530

    Add files via upload

---
+++ c++03.cpp
@@ -0,0 +1,27 @@
+// WAP to print the value of variables?
+#include<iostream>
+using namespace std;
+
+int glo=6;
+void sum(){
+    int a;
+    cout<<"The value of function glo is:"<<glo<<endl;
+}
+int main()
+{
+    int glo=9;
+    glo=89;
+    int a=14, b=67;
+    float pi=3.14;
+    char g='d';
+    bool is_t=true;
+
+    sum();
+
+    cout<<"The value of main glo is:"<<glo<<endl<<"The value of boolean is:"<<is_t<<endl;
+    cout<<"This is tutorial."<<endl<<"Here the value of a is:"<<a<<endl<<"The value of b is:"<<b<<endl;
+    cout<<"The value of pi is:"<<pi<<endl;
+    cout<<"The value of g is:"<<g<<endl;
+
+    return 0;
+}
