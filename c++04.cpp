cbd9eb90f520c0e042e89d368364effeb18446b5
Karan Veer Pal <karanpalveer@gmail.com>
2024-07-24 16:59:48 +0530

    Add files via upload

---
+++ c++04.cpp
@@ -0,0 +1,16 @@
+//WA basic program to input/output?
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num_1, num_2;
+
+    cout<<"Enter the value of number 1:"<<endl;
+    cin>>num_1;
+
+    cout<<"Enter the value of number 2:"<<endl;
+    cin>>num_2;
+
+    cout<<"The value of number1 and number2 is: "<<endl<<num_1<<endl<<num_2<<endl;
+}
