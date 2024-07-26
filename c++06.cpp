a8f02f1951eb51fd188fba0b09c8c3e73c7eacfa
Karan Veer Pal <karanpalveer@gmail.com>
2024-07-26 12:19:06 +0530

    Add files via upload

---
+++ c++06.cpp
@@ -0,0 +1,30 @@
+//WAP to access the global variable from main()?
+
+#include<iostream>
+using namespace std;
+
+int c=45;
+int main()
+{
+    int a, b, c;
+    cout<<"Enter the value of a and b:"<<endl;
+    cin>>a>>b;
+
+    //Another global variable:
+    {
+        int x=67;
+    }
+
+    c=a+b;
+    cout<<"The sum is:"<<c<<endl;
+
+    cout<<"The global c is:"<<::c;
+
+
+
+
+    //Doubt-------------------------------------------------->>
+    // cout<<"The another global x is:"<<::x;
+
+    return 0;
+}
