73ac0c45b31b3e1926468b56a54b8e1c5fbd4edb
Karan Veer Pal <karanpalveer@gmail.com>
2024-07-28 08:43:30 +0530

    Add files via upload

---
+++ c++08.cpp
@@ -0,0 +1,15 @@
+//WAP on a reference variables?
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x=455;
+    int &y = x;  //-->> correct   
+    // int y;  -->> correct
+    // y = x;  -->> correct
+
+    cout<<"The value of x is: "<<x<<endl;
+    cout<<"The value of y is: "<<y<<endl;
+}
