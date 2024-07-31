faaee157f31987a4639995d5a7d344ced9a9e61e
Karan Veer Pal <karanpalveer@gmail.com>
2024-07-31 11:43:09 +0530

    Update c++11.cpp

--- c++11.cpp
+++ c++11.cpp
@@ -4,20 +4,26 @@ i) endl; --> new line
 ii) setw --> for right justified alignment.
 */
 #include<iostream>
-#include<iomanip >
+#include<iomanip>
 using namespace std;
 
 int main()
 {
-    int a=3, b=56, c=9998;
+    int a=3, b=56, c=786, d=9998, e=546, f=5463;
 
     cout<<"The value of a without setw is: "<<a<<endl;
     cout<<"The value of b without setw is: "<<b<<endl;
     cout<<"The value of c without setw is: "<<c<<endl;
+    cout<<"The value of d without setw is: "<<d<<endl;
+    cout<<"The value of e without setw is: "<<e<<endl;
+    cout<<"The value of f without setw is: "<<f<<endl;
 
     cout<<"The value of a with setw is: "<<setw(4)<<a<<endl;
     cout<<"The value of b with setw is: "<<setw(4)<<b<<endl;
     cout<<"The value of c with setw is: "<<setw(4)<<c<<endl;
+    cout<<"The value of d with setw is: "<<setw(4)<<d<<endl;
+    cout<<"The value of e with setw is: "<<setw(3)<<e<<endl;
+    cout<<"The value of f with setw is: "<<setw(2)<<f<<endl;
 
     return 0;
 }
