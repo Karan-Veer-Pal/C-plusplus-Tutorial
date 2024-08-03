8b623768bb753fb89da1ce8b4c89bf502339d96b
Karan Veer Pal <karanpalveer@gmail.com>
2024-08-03 11:04:25 +0530

    Add files via upload

---
+++ c++14.cpp
@@ -0,0 +1,33 @@
+//WAP using all loop in c++?
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n=5;
+
+    //FIRST--->>
+    for(int i=0; i<n ; i++)
+    {
+        cout<<"For loop:"<<i<<endl;
+    }
+
+    //SECOND--->>
+    int i=0;
+    while(i<n)
+    {
+        cout<<"While loop:"<<i<<endl;
+        i++;
+    }
+
+
+    //THIRD--->>
+    int j=0;
+    do
+    {
+        cout<<"Do While loop:"<<j<<endl;
+        j++;
+    }while(j<=n);
+
+    return 0;
+}
