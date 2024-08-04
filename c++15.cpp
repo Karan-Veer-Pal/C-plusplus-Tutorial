eb08fec5af2e27acf25b1a70b586aeb812d51f7b
Karan Veer Pal <karanpalveer@gmail.com>
2024-08-04 16:35:09 +0530

    Add files via upload

---
+++ c++15.cpp
@@ -0,0 +1,22 @@
+//WAP to print multiplication table by the user using do while loop?
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int table;
+
+    cout<<"Enter any number which you wants to see a multiplication table: "<<endl;
+    cin>>table;
+
+    int i;
+    cout<<"The multiplication table is: "<<endl;
+    i=1;
+    do
+    {
+        cout<<table<<" x "<<i<<" = "<<table*i<<endl;
+        i++;
+    } while (i<=10);
+       
+    return 0;
+}
