c9fe44d740ab663c3cf70ed18f4e7356099cfa61
Karan Veer Pal <karanpalveer@gmail.com>
2024-08-05 11:48:59 +0530

    Add files via upload

---
+++ c++16.cpp
@@ -0,0 +1,28 @@
+//WAP to demonstrate the break and continue keyword?
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"The working of break is: "<<endl;
+    for (int i = 0; i < 4; i++)
+    {
+        if (i==2)
+        {
+            break;
+        }
+         cout<<i<<endl;
+    }
+
+    cout<<"The working of continue is: "<<endl;
+    for(int i = 0; i < 4; i++)
+    {
+        if (i!=2)
+        {
+            continue;
+        }
+        cout<<i<<endl;
+    }
+    
+    return 0;
+}
