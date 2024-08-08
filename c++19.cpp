ae6ba0f1ad783f3ef60c2202fd45bc063779896e
Karan Veer Pal <karanpalveer@gmail.com>
2024-08-08 11:24:03 +0530

    Add files via upload

---
+++ c++19.cpp
@@ -0,0 +1,21 @@
+//WAP to demonstrate the pointer with array?
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[]={34,56,57,12};
+    int *p=arr;
+
+    cout<<*p<<endl;
+    // cout<<*(p++)<<endl;
+    // cout<<*(++p)<<endl;
+
+    cout<<"The value of array is: "<<endl;
+    cout<<"The value of *p is: "<<*p<<endl;
+    cout<<"The value of *(p+1) is: "<<*(p+1)<<endl;
+    cout<<"The value of *(p+2) is: "<<*(p+2)<<endl;
+    cout<<"The value of *(p+3) is: "<<*(p+3)<<endl;
+
+    return 0;
+}
