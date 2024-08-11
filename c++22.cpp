b57ed7db4f17bd7005873755654717c5ef5dfd88
Karan Veer Pal <karanpalveer@gmail.com>
2024-08-11 13:54:49 +0530

    Add files via upload

---
+++ c++22.cpp
@@ -0,0 +1,26 @@
+// WAP to demonstate the concept of call by reference?
+#include<iostream>
+using namespace std;
+
+int swap(int *a, int *b); 
+
+int main()
+{
+    int x, y;
+
+    cout<<"Enter the value of x and y:"<<endl;
+    cin>>x>>y;
+
+    cout<<"The value of x: "<<x<<" and y: "<<y<<endl;
+    swap(&x, &y);
+    cout<<"The value of x: "<<x<<" and y: "<<y<<" *after function calling"<<endl;
+    return 0;
+}
+int swap(int *a, int *b)
+{
+    int temp;
+    temp=*a;
+    *a=*b;
+    *b=temp;
+    cout<<"The value of x: "<<*a<<" and y: "<<*b<<" *inside function"<<endl;
+}
