3d0d6e6bc82e6ced03222210fd5386b35b258991
Karan Veer Pal <karanpalveer@gmail.com>
2024-08-13 21:05:00 +0530

    Add files via upload

---
+++ c++24.cpp
@@ -0,0 +1,22 @@
+//WAP based on c++ reference variable? 
+#include<iostream>
+using namespace std;
+
+int &swapreference(int &u)
+{
+    return u;
+}
+int main()
+{
+     int i;
+
+    cout<<"Enter the value of i:"<<endl;
+    cin>>i;
+    cout<<"The value of i is:"<<i<<endl;
+    swapreference(i);
+    cout<<"The value of i is:"<<i<<endl;
+    swapreference(i)=566;
+    cout<<"The value of i is:"<<i<<endl;
+
+    return 0;
+}
