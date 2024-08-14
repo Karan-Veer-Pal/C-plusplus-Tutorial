ff32f000991d43c1ffaabfcd7cc1d992179b155c
Karan Veer Pal <karanpalveer@gmail.com>
2024-08-14 16:53:20 +0530

    Add files via upload

---
+++ c++25.cpp
@@ -0,0 +1,18 @@
+//WAP to demonstrate the concept of inline function?
+#include<iostream>
+using namespace std;
+
+//inline use when function body must be of 1 or 2 line.
+inline int product(int x, int y)
+{
+    return x*y;
+}
+int main()
+{
+    int a, b;
+
+    cout<<"Enter the value of a and b:"<<endl;
+    cin>>a>>b;
+    cout<<"The value of the product is: "<<product(a,b)<<endl;
+    return 0;
+}
