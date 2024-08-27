ed97d7a18a3554b3d546559e8fd6c627a59709d7
Karan Veer Pal <karanpalveer@gmail.com>
2024-08-27 14:53:35 +0530

    Add files via upload

---
+++ c++38.cpp
@@ -0,0 +1,44 @@
+//WAP on simple example of friend function?
+#include<iostream>
+using namespace std;
+
+class B;
+class A
+{
+    int data;
+
+    public:
+        void getdata(int value)
+        {
+            data=value;
+        }
+        friend void add(A,B);
+};
+
+class B
+{
+    int number;
+
+    public:
+        void getdata(int value)
+        {
+            number=value;
+        }
+        friend void add(A,B);
+};
+
+void add(A o1, B o2)
+{
+    cout<<"Summings datas of A and B objects gives me: "<<o1.data+o2.number<<endl;
+}
+int main()
+{
+    A x1;
+    x1.getdata(3);
+
+    B x2;
+    x2.getdata(9);
+
+    add(x1,x2);
+    return 0;
+}
