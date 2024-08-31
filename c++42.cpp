3794130c586e9cbd8f2acba1afaac98256265553
Karan Veer Pal <karanpalveer@gmail.com>
2024-08-31 16:51:10 +0530

    Add files via upload

---
+++ c++42.cpp
@@ -0,0 +1,30 @@
+//WAP to demostrate of the comcept of parameterized constructor by explicit call? 
+#include<iostream>
+using namespace std;
+
+class complex
+{
+    int a, b;
+
+    public:
+        //Parameterized constructor
+        complex(int x, int y);
+
+        void putdata()
+        {
+            cout<<"Your number is: "<<a<<" + "<<b<<"i"<<endl;
+        }
+};
+
+complex::complex(int x, int y)
+{
+    a=x;
+    b=y;
+};
+int main()
+{
+    //explicit call
+    complex c1 = complex(8,9);
+    c1.putdata();
+    return 0;
+}
