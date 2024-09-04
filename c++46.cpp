27a2b44b5fc92084eed6fc78700f6d4cf27ad4f6
Karan Veer Pal <karanpalveer@gmail.com>
2024-09-04 12:38:49 +0530

    Update c++46.cpp

--- c++46.cpp
+++ c++46.cpp
@@ -1,4 +1,5 @@
-//WAP to demonstrate the concept of default argument constructors?
+// Day : WAP to demonstrate the concept of default argument constructors?
+
 #include<iostream>
 using namespace std;
 
@@ -24,5 +25,6 @@ int main()
 {
     simple s1(4); //simple s1(4,45,67); --> precedence level is high as compare to default argument constructor.
     s1.putdata();
+    
     return 0;
 }
