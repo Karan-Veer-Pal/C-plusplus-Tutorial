e31a2d1110457d369af3357b1e7ee1f49760a0a0
Karan Veer Pal <karanpalveer@gmail.com>
2024-09-07 14:38:09 +0530

    Add files via upload

---
+++ c++49.cpp
@@ -0,0 +1,36 @@
+//WAP to demonstrate the concept of destructor? 
+#include<iostream>
+using namespace std;
+
+// Destructor never takes an argument nor does it return any value 
+
+int count=0;
+class number
+{
+    public:
+        number()
+        {
+            count++;
+            cout<<"This is a time when my constructor is called for object number."<<endl;
+        }
+        ~number()
+        {
+            cout<<"This is the time when my destructor is called for object number."<<count<<endl;
+            count--;
+        }
+};
+int main()
+{
+    cout<<"We are inside our main function."<<endl;
+    cout<<"Creating first objects n1"<<endl;
+    number n1;
+    {
+        cout<<"Entering this block"<<endl;
+        cout<<"Creating two more objects"<<endl;
+        number n2, n3;
+        cout<<"Exiting this block"<<endl;
+    }
+    cout<<"Back to main"<<endl;
+    
+    return 0;
+}
