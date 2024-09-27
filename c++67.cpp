b78042cc5da67400f9b5e7a75a2f7151606ac209
Karan Veer Pal <karanpalveer@gmail.com>
2024-09-27 19:23:48 +0530

    Add files via upload

---
+++ c++67.cpp
@@ -0,0 +1,41 @@
+//WAP to demonstrate the concept of this-pointer in c++?
+#include<iostream>
+using namespace std;
+
+class A
+{
+    int a;
+    int b;
+    public:
+        void setdata_a(int a)
+        {
+            // this-pointer operator is used when a variable name is same in class and function. 
+            this->a = a;
+        }
+        void getdata_a(void)
+        {
+            cout<<"The value of a is: "<<a<<endl;
+        }
+
+        A& setdata_b(int b)   //this will return the object
+        {
+            this->b = b;
+            return *this;
+        }
+        void getdata_b(void)
+        {
+            cout<<"The value of b is: "<<b<<endl;
+        }
+};
+int main()
+{
+    // this is a keyword which is a pointer which points to the object which invokes the member function
+    A a;
+    a.setdata_a(5);
+    a.getdata_a();   
+
+    A b;
+    b.setdata_a(50);
+    b.getdata_b();   
+    return 0;
+}
