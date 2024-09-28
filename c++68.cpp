e892c68e846bc7eebc2017b0b545b380a4144d80
Karan Veer Pal <karanpalveer@gmail.com>
2024-09-28 13:10:24 +0530

    Add files via upload

---
+++ c++68.cpp
@@ -0,0 +1,12 @@
+//WAP to demonstrate the concept of polymorphism?
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    //Polymorphism 
+    //-->> one name and multiple form
+    //-->> eg. function overloading and operator overloading
+    //-->> virtual function
+    return 0;
+}
