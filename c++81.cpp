76e077923d9d0234d7f7e96f25a57c2f4013f3b6
Karan Veer Pal <karanpalveer@gmail.com>
2024-10-11 11:27:09 +0530

    Add files via upload

---
+++ c++81.cpp
@@ -0,0 +1,27 @@
+//WAP to find the average?
+#include<iostream>
+using namespace std;
+
+float funcAverage1(int a, int b)
+{
+    float avg = (a+b)/2.0;
+    return avg;
+}
+
+float funcAverage2(int a, float b)
+{
+    float avg = (a+b)/2.0;
+    return avg;
+}
+
+int main()
+{
+    float a; 
+    a =funcAverage1(5,2);              
+    cout<<"The value of average 1 of "<< a <<endl;
+
+    float b; 
+    b =funcAverage2(5,2.4);              
+    cout<<"The value of average 2 of "<< b <<endl;
+    return 0;
+}
